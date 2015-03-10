a7f77398314f1be6f950b08434e733e2192fc89e
jackyyang09 <jackyyang267@gmail.com>
2015-03-09 21:25:42 -0400

    Create Nexus.java

---
+++ Nexus.java
@@ -0,0 +1,30 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * The Nexus, does a bunch of things
+ * 
+ * @Jacky Yang
+ * @Day 1
+ */
+public class Nexus extends Buildings
+{
+    private int gold;
+    private int goldGen;
+    /**
+     * Act method
+     */
+    public void act() 
+    {
+        if (goldGen == 2){gold++;} //Generates gold every 2 acts
+        barracksGen();
+    }    
+    
+    private barracksGen()
+    {
+        if (gold >= 1000)
+        {
+            getWorld().spawnBuilding();
+            gold -= 1000;
+        }
+    }
+}
