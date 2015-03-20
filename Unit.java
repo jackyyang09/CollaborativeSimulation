3abea44b55f165aba1ea7db7a674b6175a3cca0b
krisleung98 <krisleung98@gmail.com>
2015-03-20 15:40:14 -0400

    Update Unit.java

--- Unit.java
+++ Unit.java
@@ -1,9 +1,10 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.ArrayList;
 
 /**
  * Unit is a unit.
  * 
- * @author Kris Leung
+ * @author Kris Leung, (some code cited from Mr.Cohen's bug example)
  * @version Mar 2015
  */
 public abstract class Unit extends Actor
@@ -18,8 +19,135 @@ public abstract class Unit extends Actor
     protected int xValue; //x value of the unit
     protected int yValue; //y value of the unit
     protected boolean side; //true if playerOne side and false if playerTwo side
-    Buildings building = new Buildings();
-    Buildings building2 = new Buildings();
+    protected int counter;
+    Buildings building;
+    Buildings building2;
+    private Unit targetUnit;
+    private ArrayList<Unit> unit;
+    private Buildings targetBuildings;
+    private ArrayList<Buildings> buildings;
+    
+    /**
+     * Act - do whatever the Bug wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        counter++;
+        if (currentHp > 0)
+        {
+            if (counter == 5) // Only run every 5 acts to avoid lag
+            {
+                targetClosestUnit (this.side);
+                //hpBar.update(hp);
+            }
+            if (targetUnit != null && targetUnit.getWorld() != null)
+            {
+                moveTowardOrAttackUnit();
+            }
+            else
+            {
+                moveForward();
+            }
+        }
+        // Death:
+        else
+        {
+            Map m = (Map)getWorld();
+            getWorld().removeObject(this);
+        }
+    }  
+    
+    /**
+     * Private method, called by act(), that constantly checks for closer targets
+     */
+    private void targetClosestUnit (boolean whichSide)
+    {
+        double closestTargetDistance = 0;
+        double distanceToActor;
+        int numUnit;
+        // Get a list of all Units in the World, cast it to ArrayList for easy management
+
+        numUnit = getWorld().getObjects(Unit.class).size();
+        // If any units are found
+        if (numUnit > 50)
+        {
+            unit = (ArrayList)getObjectsInRange(100, Unit.class);
+        }
+        else if (numUnit > 20)
+        {
+            unit = (ArrayList)getObjectsInRange(200, Unit.class);
+        }
+        else
+            unit = (ArrayList)getWorld().getObjects(Unit.class);
+
+        if (unit.size() > 0)
+        {
+            // set the first one as my target
+            do{
+                int whichUnit = 0;
+                targetUnit = unit.get(whichUnit);
+                if (targetUnit.getSide() == this.side)
+                {
+                    whichUnit++;
+                }
+                else{
+                    break;
+                }
+            }
+            while(true);
+            // Use method to get distance to target. This will be used
+            // to check if any other targets are closer
+            closestTargetDistance = Map.getDistance (this, targetUnit);
+
+            // Loop through the objects in the ArrayList to find the closest target
+            for (Unit o : unit)
+            {
+                // Cast for use in generic method
+                Actor a = (Actor) o;
+                // Measure distance from me
+                distanceToActor = Map.getDistance(this, a);
+                // If I find a Unit closer than my current target, I will change
+                // targets
+                if (distanceToActor < closestTargetDistance)
+                {
+                    targetUnit = o;
+                    closestTargetDistance = distanceToActor;
+                }
+            }
+        }
+    }
+
+    /**
+     * Private method, calle by act(), that moves toward the target,
+     * or eats it if within range.
+     */
+    private void moveTowardOrAttackUnit ()
+    {
+        turnTowards(targetUnit.getX(), targetUnit.getY());
+
+        if (this.getNeighbours (30, true, Unit.class).size() > 0)
+        {
+            // Deal damage to enemy unit
+            targetUnit.dealDamage(damage);
+
+            //attacking animation
+        }
+        else
+        {
+            move (speed);
+        }
+    }
+
+    private void moveForward ()
+    {
+        if (this.side == true)
+        {
+            move (speed);
+        }
+        else
+            move (-speed);
+    }
     
     public void pathFinding(int x, int y){
         turnTowards(x, y);
