04961dad19a8930116c4e5f39c2c80d5751900b2
Cypheruim <ryanhuang1234@hotmail.com>
2015-03-20 14:16:05 -0400

    Update Nexus.java

--- Nexus.java
+++ Nexus.java
@@ -8,38 +8,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Nexus extends Buildings
 {
-    private int gold;
-    private int goldGen;
-    /**
-     * Act method
-     */
-    public void act() 
+    public void act()
     {
-        goldGen();
-    }
-
-    public void goldGen() 
-    {
-        if (goldGen == 2)
-        {
-            gold++;
-            goldGen = 0;
-        } //Generates gold every 2 acts
-        goldGen++;
-    }
-
-    public int getGold()
-    {
-        return gold;
-    }
-    
-    public boolean setGold(int gold){
-        if(gold>=0){
-            amountGold += gold;
-            return true;
-        }
-        else{
-            return false;
-        }
+        
     }
 }
