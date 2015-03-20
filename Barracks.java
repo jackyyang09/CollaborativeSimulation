f8d6c6e5a5e0dfc53f91b8a36ddc565be73b03ba
jackyyang09 <jackyyang267@gmail.com>
2015-03-19 20:52:07 -0400

    Create Barracks.java

---
+++ Barracks.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Does Barracks-like things
+ * 
+ * @Jacky
+ * @Day 5 WIP
+ */
+public class Barracks extends Buildings
+{
+    /**
+     * Act - do whatever the Barracks wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
