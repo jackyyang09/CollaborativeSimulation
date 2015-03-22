d0d341e328362f44dd51e81d5425b0bcb1d04300
jackyyang09 <jackyyang267@gmail.com>
2015-03-22 16:17:56 -0400

    Update Barracks.java

--- Barracks.java
+++ Barracks.java
@@ -1,19 +1,38 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Does Barracks-like things
+ * Does what a Barracks does
  * 
- * @Jacky
- * @Day 5 WIP
+ * @author Jacky Yang
+ * @version Day X WIP
  */
 public class Barracks extends Buildings
 {
+    public Barracks()
+    {
+        buildTime = 500;
+    }
+    
     /**
      * Act - do whatever the Barracks wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
     {
-        // Add your action code here.
-    }    
+        if (buildTime > 0)
+        {
+            setImage("construction1.png");
+            buildTime--;
+        }
+        if (buildTime == 0){setImage("building1.png");}
+    }
+    
+    /**
+     * Method that is called when Barracks is destroyed
+     * Will likely decrease a value within the player class
+     */
+    public void destroy()
+    {
+        //Code goes here
+    }
 }
