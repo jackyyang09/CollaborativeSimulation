f8687aca875a50adf9e005488f7a0ce9c65848ee
jackyyang09 <jackyyang267@gmail.com>
2015-03-22 16:18:13 -0400

    Update WarFactory.java

--- WarFactory.java
+++ WarFactory.java
@@ -8,12 +8,22 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class WarFactory extends Buildings
 {
+    public WarFactory()
+    {
+        buildTime = 750;
+    }
+    
     /**
      * Act - do whatever the WarFactory wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
     {
-        // Add your action code here.
+        if (buildTime > 0)
+        {
+            setImage("construction2.png");
+            buildTime--;
+        }
+        if (buildTime == 0){setImage("building3.png");}
     }    
 }
