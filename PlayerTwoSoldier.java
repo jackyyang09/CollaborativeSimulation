d5ffe9b4bcdd85100aa837f4fdd43dec2ce21243
jackyyang09 <jackyyang267@gmail.com>
2015-03-30 12:04:37 -0400

    Update PlayerTwoSoldier.java

--- PlayerTwoSoldier.java
+++ PlayerTwoSoldier.java
@@ -16,4 +16,14 @@ public class PlayerTwoSoldier extends Soldier
     public void act(){
         target(false);
     }
+    
+    public void animate()
+    {
+        animTimer++;
+        if (animTimer == 0){setImage("sildier0.png");}
+        if (animTimer == 15){setImage("sildier1.png");}
+        if (animTimer == 30){setImage("sildier2.png");}
+        if (animTimer == 45){setImage("sildier1.png");}
+        if (animTimer == 59){animTimer = -1;}
+    }
 }
