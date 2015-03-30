d423bb16734795245290edc9f4b4e2fbf94858b1
jackyyang09 <jackyyang267@gmail.com>
2015-03-30 12:05:06 -0400

    Update PlayerOneSoldier.java

--- PlayerOneSoldier.java
+++ PlayerOneSoldier.java
@@ -3,7 +3,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class PlayerOneSoldier here.
  * 
- * @author (your name) 
+ * @author Jacky Yang
  * @version (a version number or a date)
  */
 public class PlayerOneSoldier extends Soldier
@@ -14,5 +14,15 @@ public class PlayerOneSoldier extends Soldier
     }
     public void act(){
         target(true);
+    }
+    
+    public void act(){
+        target(true);
+        animTimer++;
+        if (animTimer == 0){setImage("goldier0.png");}
+        if (animTimer == 15){setImage("goldier1.png");}
+        if (animTimer == 30){setImage("goldier2.png");}
+        if (animTimer == 45){setImage("goldier1.png");}
+        if (animTimer == 59){animTimer = -1;}
     }
 }
