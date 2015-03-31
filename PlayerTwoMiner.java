6d82ca4315abf004dfe6c2c0b0ab4da84363425e
krisleung98 <krisleung98@gmail.com>
2015-03-31 07:31:18 -0400

    Update PlayerTwoMiner.java

--- PlayerTwoMiner.java
+++ PlayerTwoMiner.java
@@ -6,34 +6,22 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class PlayerTwoMiner extends Miner
+public class PlayerTwoMiner extends PlayerTwo
 {
     public PlayerTwoMiner(GoldMine g1, GoldMine g2, Nexus n, Player p){
         nexus = n;
         player = p;
         gMine1 = g1;
         gMine2 = g2;
-        prepare();
+        prepareMiner();
         side = false;
     }
 
     public void act(){
         target(getGoldMine(), false);
+        healthBarMiner.setCurrentHp(currentHp);
         if (goldCarry == 0){setImage("goldminer0.png");}
         if (goldCarry == 50){setImage("goldminer1.png");}
         if (goldCarry == 100){setImage("goldminer2.png");}
-    }
-
-    public GoldMine getGoldMine(){
-        if(wentOnce == true){
-            if(Greenfoot.getRandomNumber(2) == 0){
-                gMine = gMine1;
-            }
-            else{
-                gMine = gMine2;
-            }
-            wentOnce = false;
-        }
-        return gMine;
     }
 }
