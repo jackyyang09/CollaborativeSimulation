9930034b6d2233cb842a9597779d6d8ad13fe990
krisleung98 <krisleung98@gmail.com>
2015-03-31 11:10:47 -0400

    Update GoldMine.java

--- GoldMine.java
+++ GoldMine.java
@@ -1,27 +1,115 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
 /**
- * Write a description of class PlayerOneMiner here.
+ * A goldmine for units to collect gold.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author (Kajamugesh Raneethran) 
+ * @version (February 29th, 2015)
  */
-public class PlayerOneMiner extends PlayerOne
+public class GoldMine extends Buildings
 {
-    public PlayerOneMiner(GoldMine g1, GoldMine g2, Nexus n, Player p){
-        nexus = n;
-        player = p;
-        gMine1 = g1;
-        gMine2 = g2;
-        prepareMiner();
-        side = true;
+    protected int amount;//current amount of gold in the mine
+    protected boolean propertyOne;//true if controlled by player one
+    protected boolean propertyTwo;//true if controlled by player two
+    protected int maxAmount;//maximum amount of gold that can be in the mine
+    protected int regen;//amount of time required for gold to be added
+    protected int time = 0;//time left until gold is added to mine
+    public GoldMine()
+    {
+        maxAmount= 5000;
+        amount = 3000;
+        propertyOne = false; 
+        propertyTwo = false;
+        regen = 10;
     }
 
-    public void act(){
-        targetGoldMine(getGoldMine(), true);
-        healthBarMiner.setCurrentHp(currentHp);
-        if (goldCarry == 0){setImage("miner0.png");}
-        if (goldCarry == 50){setImage("miner1.png");}
-        if (goldCarry == 100){setImage("miner2.png");}
+    /**
+     * Act - do whatever the Gold wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     * regenerates gold if enough acts have passed
+     */
+    public void act() 
+    {
+        if (time == regen)
+        {
+            time = 0;
+            subAmount(-1);
+        }
+        else
+        {
+            time++;   
+        }
+    } 
+
+    /**
+     * sets amount of gold in the gold mine
+     * @param amount     amount
+     */
+    public void setAmount(int amount)
+    {
+        if (amount<0)
+        { 
+            amount = this.amount;
+        }
+    }
+
+    /**
+     * subtracts amount of gold, or adds if negative, in the gold mine
+     * doesn't allow negative  current amounts or past max amount
+     * @param amount     amount taken
+     */
+    public boolean subAmount(int amount)
+    {
+        int request = amount - this.amount;
+        if (request < 0)
+        {
+            return false;
+        }
+        else if (request > maxAmount)
+        {
+            amount = maxAmount;
+            return true;
+        }
+        else
+        {
+            amount = request;
+            return true;
+        }
+    }
+
+    /**
+     * returns amount of gold in the gold mine
+     */
+    public int getAmount()
+    {
+        return amount;
+    }
+
+    /**
+     * sets regeneration rate
+     * @param rate     the larger the number the longer
+     */
+    public void regenRate(int rate)
+    {
+        regen = rate;
+    }
+
+    /**
+     * decides which player gets control
+     * over the gold mine
+     * @param player     player one gets control if 1, player two gets control if 2
+     */
+    public void control(int player)
+    {
+        if (player == 1)
+        {
+            propertyOne = true;
+            propertyTwo = false;
+        }
+
+        if (player == 2)
+        {
+            propertyOne = false;
+            propertyTwo = true;
+        }
     }
 }
