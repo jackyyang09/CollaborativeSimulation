ad50252e101b902d7b5914d46de3d593c10c44e0
krisleung98 <krisleung98@gmail.com>
2015-03-31 07:30:14 -0400

    Update PlayerOneMiner.java

--- PlayerOneMiner.java
+++ PlayerOneMiner.java
@@ -6,34 +6,21 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class PlayerOneMiner extends Miner
+public class PlayerOneMiner extends PlayerOne
 {
     public PlayerOneMiner(GoldMine g1, GoldMine g2, Nexus n, Player p){
         nexus = n;
         player = p;
         gMine1 = g1;
         gMine2 = g2;
-        prepare();
+        prepareMiner();
         side = true;
     }
 
     public void act(){
         target(getGoldMine(), true);
+        healthBar.setCurrentHp(currentHp);
         if (goldCarry == 0){setImage("miner0.png");}
         if (goldCarry == 50){setImage("miner1.png");}
         if (goldCarry == 100){setImage("miner2.png");}
     }
-
-    public GoldMine getGoldMine(){
-        if(wentOnce == true){
-            if(Greenfoot.getRandomNumber(2) == 0){
-                gMine = gMine1;
-            }
-            else{
-                gMine = gMine2;
-            }
-            wentOnce = false;
-        }
-        return gMine;
-    }
-}
