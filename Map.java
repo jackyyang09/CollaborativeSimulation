a04bc63403a7818199c6ee8fa6acfc55b8bec3d4
krisleung98 <krisleung98@gmail.com>
2015-03-31 18:28:30 -0400

    Update Map.java

--- Map.java
+++ Map.java
@@ -128,11 +128,11 @@ public class Map extends World
         else if(code == 1){
             if(side == true){
                 //true for player 1
-                addObject(new PlayerOneSoldier(nexusTwo), 225, 320);
+                addObject(new PlayerOneSoldier(nexusTwo), 251, 320);
                 playerOne.addSoldiers(1);
             }
             else{
-                addObject(new PlayerTwoSoldier(nexusOne), 735, 320);
+                addObject(new PlayerTwoSoldier(nexusOne), 709, 320);
                 playerTwo.addSoldiers(1);
             }   
         }
