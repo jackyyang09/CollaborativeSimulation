787a90f215208d1d5ec7c5d523d4d9e672e0de43
jackyyang09 <jackyyang267@gmail.com>
2015-03-31 11:57:45 -0400

    Update Nexus.java

--- Nexus.java
+++ Nexus.java
@@ -8,13 +8,13 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Nexus extends Buildings
 {
-    protected HealthBar healthBar = new HealthBar(50000, 50000, 10000, -75, 30, 5, this);
-    
+    private HealthBar healthBar = new HealthBar(50000, 50000, 10000, 30, 5, this);
+    private boolean prepare = false;
     public void addedToWorld (World w)
     {
         getWorld().addObject (healthBar, getX(), getY());
     }
-    
+
     public Nexus(int player)
     {
         graphic = player;
@@ -24,7 +24,29 @@ public class Nexus extends Buildings
 
     public void act()
     {   
-        if (graphic == 1){setImage("golding2.png");}
+        if (prepare == false){prepare();}
         regen();  
+    }
+    
+    public void prepare()
+    {
+        if (graphic == 1){setImage("golding2.png");}
+        if (graphic == 2){getImage().mirrorHorizontally);}
+        prepare = true;
+    }
+
+    /**
+     * Deal amount of damage to the Nexus
+     * @param dmg      Damage dealt to the unit
+     * @return boolean   Returns true if value is changed and false otherwise
+     */
+    public boolean dealDamage (int dmg){
+        if (dmg >= 0){
+            health -= dmg;
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 }
