374af7d8ecb2fd4b9f44da6f1bc96181691192f8
krisleung98 <krisleung98@gmail.com>
2015-03-31 07:30:30 -0400

    Update PlayerOneMiner.java

--- PlayerOneMiner.java
+++ PlayerOneMiner.java
@@ -19,7 +19,7 @@ public class PlayerOneMiner extends PlayerOne
 
     public void act(){
         target(getGoldMine(), true);
-        healthBar.setCurrentHp(currentHp);
+        healthBarMiner.setCurrentHp(currentHp);
         if (goldCarry == 0){setImage("miner0.png");}
         if (goldCarry == 50){setImage("miner1.png");}
         if (goldCarry == 100){setImage("miner2.png");}
