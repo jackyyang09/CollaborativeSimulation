8a215b679fb4255893ee9c228c1090a0f801ce1c
krisleung98 <krisleung98@gmail.com>
2015-04-01 00:38:54 -0400

    Update WarFactory.java

--- WarFactory.java
+++ WarFactory.java
@@ -8,10 +8,18 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class WarFactory extends Buildings
 {
+    private HealthBar healthBar = new HealthBar(10000, 10000, 2000, 0, 50, 15, this);
+    public void addedToWorld (World w)
+    {
+        getWorld().addObject (healthBar, getX(), getY());
+    }
+    
     public WarFactory(int player)
     {
         graphic = player;
         buildTime = 750;
+        health = 10000;
+        maxHealth = 10000;
     }
     
     /**
@@ -28,5 +36,11 @@ public class WarFactory extends Buildings
         if (buildTime > 0){buildTime--;}
         if (buildTime == 0){removeTouching(Graphics.class);}
         regen();
+        if(health > 0){
+            healthBar.setCurrentHp(health);
+        }
+        else{
+            getWorld().removeObject(this);
+        }
     }    
 }
