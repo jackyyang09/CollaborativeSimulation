fdf95c0639249c3af198a269e913e1c570f9f0fd
jackyyang09 <jackyyang267@gmail.com>
2015-04-01 22:46:12 -0400

    Update PlayerTwoSoldier.java

--- PlayerTwoSoldier.java
+++ PlayerTwoSoldier.java
@@ -1,7 +1,7 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class PlayerTwoSoldier here.
+ * Player Two's Soldier
  * 
  * @author Jacky Yang
  * @version (a version number or a date)
