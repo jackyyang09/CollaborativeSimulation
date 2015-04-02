51f801dbb719cf4da215cf8b6c052fa957b74ccb
jackyyang09 <jackyyang267@gmail.com>
2015-04-02 00:07:02 -0400

    Update Barracks.java

--- Barracks.java
+++ Barracks.java
@@ -26,6 +26,50 @@ public class Barracks extends Buildings
      */
     public void act() 
     {
+        import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Does what a Barracks does
+ * 
+ * @author Jacky Yang
+ * @version Final
+ */
+public class Barracks extends Buildings
+{
+
+    /**
+     * Does what a Barracks does
+     * Chooses which side 
+     * 
+     * @param player 1 if player one, 2 if player two
+     */ 
+    public Barracks(int player)
+    {
+        graphic = player;
+        buildTime = 500;
+        health = 500;
+    }
+
+    /**
+     * Act - do whatever the Barracks wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        if (graphic == 1){setImage("golding1.png");}
+        regen();
+    }
+
+    /**
+     * Method that is called when Barracks is destroyed
+     * Will likely decrease a value within the player class
+     */
+    public void destroy()
+    {
+        //Code goes here
+    }
+}
+
         regen();
     }
 
