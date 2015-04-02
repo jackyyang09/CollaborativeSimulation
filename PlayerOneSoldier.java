64833ac7575656063b2a3353fc52f3c35a149405
jackyyang09 <jackyyang267@gmail.com>
2015-04-01 22:46:10 -0400

    Update PlayerOneSoldier.java

--- PlayerOneSoldier.java
+++ PlayerOneSoldier.java
@@ -1,7 +1,7 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class PlayerOneSoldier here.
+ * Player One's soldier
  * 
  * @author Jacky Yang
  * @version (a version number or a date)
